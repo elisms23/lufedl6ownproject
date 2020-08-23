2c69cc46650a7e342765d4b9483a1ef4c6036af5
Natasha <konv.42@gmail.com>
2020-08-23 14:49:30 +0300

    typo corrected, js files created

---
+++ js/slider.js
@@ -0,0 +1,50 @@
+const sliderContent = [
+    {
+        id: "project1",
+        backgroundImage: "../img/modern-living-room-1.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project2",
+        backgroundImage: "../img/modern-living-room-2.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project3",
+        backgroundImage: "../img/modern-living-room-3.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project4",
+        backgroundImage: "../img/modern-living-room-4.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project5",
+        backgroundImage: "../img/modern-living-room-5.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project6",
+        backgroundImage: "../img/modern-living-room-6.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project7",
+        backgroundImage: "../img/modern-living-room-7.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+    {
+        id: "project8",
+        backgroundImage: "../img/modern-living-room-8.png",
+        title: "Modern living room",
+        text: "Interior design",
+    },
+]
